20a46b09e227d318faec53c1fa527a3d10d879be
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-06-02 04:01:52 +0200

    tunes can be loaded now

--- app/src/main/java/xyz/oreganoli/noise/MainActivity.java
+++ app/src/main/java/xyz/oreganoli/noise/MainActivity.java
@@ -1,21 +1,22 @@
 package xyz.oreganoli.noise;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -91,9 +92,15 @@ public class MainActivity extends AppCompatActivity {
         File path = getExternalFilesDir(null);
         File file = new File(path, filename);
         RandomAccessFile raf = new RandomAccessFile(file, "rws");
-        raf.writeUTF(currentTune);
+        raf.writeChars(currentTune);
         raf.close();
         Toast.makeText(this, "Saved as " + path.getAbsolutePath() + "/" + filename, Toast.LENGTH_SHORT).show();
+    }
+    public void loadTune(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        startActivityForResult(intent, 1);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,5 +113,29 @@ public class MainActivity extends AppCompatActivity {
         updateTuneView();
         player = new NotePlayer(this);
         executor = new ThreadPoolExecutor(4, 4, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1) {
+            Uri uri = data.getData();
+            try {
+                InputStream in = getContentResolver().openInputStream(uri);
+                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                StringBuilder total = new StringBuilder();
+                for (String line; (line = r.readLine()) != null; ) {
+                    total.append(line);
+                }
+                currentTune = total.toString();
+                updateTuneView();
+                r.close();
+                in.close();
+                Toast.makeText(this, "Loaded the song " + currentTune, Toast.LENGTH_LONG).show();
+            } catch (FileNotFoundException e) {
+                Toast.makeText(this, "No such file found!", Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(this, "IO exception!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
